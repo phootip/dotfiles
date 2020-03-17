3bf5115c2c26059afb6aeffa52f587be4c448814
phootip <phootip.t@gmail.com>
2020-03-17 19:05:44 +0700

    nvim config

--- install/link.sh
+++ install/link.sh
@@ -21,4 +21,22 @@ for file in $linkables ; do
     fi
 done
 
+echo -e "\\n\\ninstalling to ~/.config"
+echo "=============================="
+if [ ! -d "$HOME/.config" ]; then
+    echo "Creating ~/.config"
+    mkdir -p "$HOME/.config"
+fi
+
+config_files=$( find "$DOTFILES/config" -maxdepth 1 2>/dev/null )
+for config in $config_files; do
+    target="$HOME/.config/$( basename "$config" )"
+    if [ -e "$target" ]; then
+        echo "~${target#$HOME} already exists... Skipping."
+    else
+        echo "Creating symlink for $config"
+        ln -s "$config" "$target"
+    fi
+done
+
 touch $HOME/.dotfiles/zsh/local.zsh
