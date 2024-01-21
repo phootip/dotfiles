17b67e201563dc3d0dbcbb101b5a28f89aee69f6
phootip <phootip@gmail.com>
2024-01-21 17:12:46 +0700

    brewPath base on OS

--- run_once_default_shell.sh
+++ run_once_default_shell.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-chsh -s $(which zsh)
+if ! [[ -x "$(command -v zsh)" ]]; then
+  echo "zsh not found. Please install and then re-run installation scripts"
+  exit 1
+fi
+
+read -rn 1 -p "Set zsh to default shell? [y/n] " ans
+if [[ $ans =~ ^([Yy])$ ]]; then
+  command -v zsh | sudo tee -a /etc/shells
+  chsh -s $(which zsh)
+fi
