e9b9900d6798e7780d5c927836b5a61768701ed5
phootip <phootip.t@7peakssoftware.com>
2024-01-30 17:08:18 +0700

    add lazyvim, lunarvim alias

--- dot_local/share/zsh/alias.zsh
+++ dot_local/share/zsh/alias.zsh
@@ -6,41 +6,44 @@ alias la='ls -lAh'
 alias loadnvm="source $NVM_DIR/nvm.sh"
 alias vim="nvim"
 export EDITOR=nvim
+alias lazyvim="NVIM_APPNAME=lazyvim nvim"
+export PATH=~/.local/bin:$PATH
 # tmux
+export LC_ALL="en_US.UTF-8"
 alias tmux="tmux -u"
 # ranger
-export LC_ALL=C
+# export LC_ALL=C
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 alias r='ranger'
 # kube
 alias first_pod="kgpo | head -2 | tail -1 | cut -d ' ' -f1"
 tmux_color() {
-for i in {0..255}; do
-  printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
-done
+  for i in {0..255}; do
+    printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
+  done
 }
 # https://unix.stackexchange.com/questions/236094/how-to-remove-the-last-command-or-current-command-for-bonus-from-zsh-history
 alias forget=' my_remove_last_history_entry'
 my_remove_last_history_entry() {
-    is_int() ( return $(test "$@" -eq "$@" > /dev/null 2>&1); )
-    history_file="${HOME}/.zsh_history"
-    history_temp_file="${history_file}.tmp"
-    line_cout=$(wc -l $history_file | awk '{print $1;}')
+  is_int() (return $(test "$@" -eq "$@" >/dev/null 2>&1))
+  history_file="${HOME}/.zsh_history"
+  history_temp_file="${history_file}.tmp"
+  line_cout=$(wc -l $history_file | awk '{print $1;}')
+  lines_to_remove=1
+  if [ $# -eq 0 ]; then
     lines_to_remove=1
-    if [ $# -eq 0 ]; then
-        lines_to_remove=1
+  else
+    if $(is_int "${1}"); then
+      lines_to_remove="$1"
     else
-        if $(is_int "${1}"); then
-            lines_to_remove="$1"
-        else
-            echo "Unknown argument passed. Exiting..."
-            return
-        fi
+      echo "Unknown argument passed. Exiting..."
+      return
     fi
-    lines_to_remove=$(($line_cout-$lines_to_remove))
-    echo $lines_to_remove
-    fc -W
-    cat $history_file | head -n "${lines_to_remove}" &> $history_temp_file
-    mv "$history_temp_file" "$history_file"
-    fc -R
+  fi
+  lines_to_remove=$(($line_cout - $lines_to_remove))
+  echo $lines_to_remove
+  fc -W
+  cat $history_file | head -n "${lines_to_remove}" &>$history_temp_file
+  mv "$history_temp_file" "$history_file"
+  fc -R
 }
