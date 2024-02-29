0562fa1a13964bdea6de7079cc086f07fe3489ed
phootip <phootip@gmail.com>
2024-02-29 22:00:57 +0700

    add default brewPath

--- run_onchange_brew_bundle.sh
+++ run_onchange_brew_bundle.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-brew bundle --no-lock --file ".local/share/chezmoi/Brewfile"
+read -rn 1 -p "Install packages with brew? [y/n] " ans
+if [[ $ans =~ ^([Yy])$ ]]; then
+	brew bundle --no-lock --file ".local/share/chezmoi/Brewfile"
+fi
