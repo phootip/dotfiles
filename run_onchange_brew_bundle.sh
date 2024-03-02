a40c4870fd44d7850bc810331d99f18537a7b386
phootip <phootip@gmail.com>
2024-03-02 12:40:53 +0700

    add syncthing

--- run_onchange_brew_bundle.sh
+++ run_onchange_brew_bundle.sh
@@ -3,3 +3,11 @@ read -rn 1 -p "Install packages with brew? [y/n] " ans
 if [[ $ans =~ ^([Yy])$ ]]; then
 	brew bundle --no-lock --file "~/.local/share/chezmoi/Brewfile"
 fi
+
+read -rn 1 -p "Enable syncthing service? [y/n] " ans
+if [[ $ans =~ ^([Yy])$ ]]; then
+	systemctl --user enable syncthing.service
+	systemctl --user start syncthing.service
+	# for dubugging: https://docs.syncthing.net/users/autostart.html#using-the-journal
+	# journalctl -e --user-unit=syncthing.service
+fi
