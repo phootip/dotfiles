074340325b9fd3a1249f04d836ba053cfa63bbc2
phootip <phootip.t@gmail.com>
2025-09-09 16:28:21 +0700

    more terraform qol

--- dot_local/share/zsh/alias.zsh
+++ dot_local/share/zsh/alias.zsh
@@ -45,6 +45,12 @@ alias tf='terraform'
 alias tfi='terraform init'
 alias tfib='terraform init -backend-config'
 alias tfp='terraform plan -lock=false -out=changes.plan'
+tfpe() {
+  terraform plan -lock=false -out=changes.plan -var-file variables.$1.tfvars
+}
+tfie() {
+  terraform init -backend-config=azure.$1.tfbackend
+} 
 alias tfa='terraform apply changes.plan'
 alias tfsl='terraform state list'
 alias tfss='terraform state show'
@@ -71,27 +77,6 @@ tmux_color() {
   done
 }
 # https://unix.stackexchange.com/questions/236094/how-to-remove-the-last-command-or-current-command-for-bonus-from-zsh-history
-alias forget=' my_remove_last_history_entry'
-my_remove_last_history_entry() {
-  is_int() (return $(test "$@" -eq "$@" >/dev/null 2>&1))
-  history_file="${HOME}/.zsh_history"
-  history_temp_file="${history_file}.tmp"
-  line_cout=$(wc -l $history_file | awk '{print $1;}')
-  lines_to_remove=1
-  if [ $# -eq 0 ]; then
-    lines_to_remove=1
-  else
-    if $(is_int "${1}"); then
-      lines_to_remove="$1"
-    else
-      echo "Unknown argument passed. Exiting..."
-      return
-    fi
-  fi
-  lines_to_remove=$(($line_cout - $lines_to_remove))
-  echo $lines_to_remove
-  fc -W
-  cat $history_file | head -n "${lines_to_remove}" &>$history_temp_file
-  mv "$history_temp_file" "$history_file"
-  fc -R
-}
+#
+alias bwa='rbw add --folder 7peaks'
+alias bwl=$'rbw list --raw | jq \'.[] | select(.folder == "7peaks") | {folder,name,user}\' | mlr --j2p --barred cat'
