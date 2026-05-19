ece7ee178e7375ead968f3b35c8479705f11d3ea
phootip.t <phootip.t@gmail.com>
2026-05-19 11:17:08 +0700

    update local.example.zsh

--- dot_local/share/zsh/local.example.zsh
+++ dot_local/share/zsh/local.example.zsh
@@ -1,2 +1,16 @@
 # copy as local.zsh
 # export FOO=bar
+
+# export PATH="$HOME/.homebrew/bin:$PATH"
+# export GEMINI_API_KEY='your-gemini-key'
+#
+# testing cert
+# export NODE_EXTRA_CA_CERTS=~/cacert.pem
+# export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
+# export PATH="$HOME/.local/share/mise/shims:$PATH"
+#
+# export OPENROUTER_API_KEY="your-openrouter-key"
+# export ANTHROPIC_BASE_URL="https://openrouter.ai/api"
+# export ANTHROPIC_AUTH_TOKEN="$OPENROUTER_API_KEY"
+# export ANTHROPIC_API_KEY="" # Important: Must be explicitly empty
+# export ANTHROPIC_MODEL="openrouter/owl-alpha"
